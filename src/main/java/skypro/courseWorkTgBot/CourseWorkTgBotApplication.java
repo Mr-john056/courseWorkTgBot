194e48877607fde5601d0dd16d5ab7dbe1a7e29b
MSI <kulakov056@mail.ru>
2024-03-10 23:19:36 +0500

    create repository

---
+++ src/main/java/skypro/courseWorkTgBot/CourseWorkTgBotApplication.java
@@ -0,0 +1,13 @@
+package skypro.courseWorkTgBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWorkTgBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseWorkTgBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/skypro/courseWorkTgBot/CourseWorkTgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package skypro.courseWorkTgBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWorkTgBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
